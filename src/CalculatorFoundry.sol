ec407958d518fc026b1103bb1c97dbc9131e5ae1
Alberto <albertofm1992@gmail.com>
2025-05-10 15:43:46 +0300

    added natspec comments

--- src/CalculatorFoundry.sol
+++ src/CalculatorFoundry.sol
@@ -26,7 +26,11 @@ contract CalculatorFoundry {
         _;
     }
 
-    // Constructor
+    /**
+     * @notice Constructor of smart contract
+     * @param result_ result of the last operation
+     * @param admin_ administrator of the smart contract
+     */
     constructor(uint256 result_, address admin_) {
         result = result_;
         admin = admin_;
@@ -34,8 +38,12 @@ contract CalculatorFoundry {
 
     // Functions
 
-    // 1. Addition
-
+    /**
+     * @notice Function to sum two numbers
+     * @param firstNumber_ first number for the addition
+     * @param secondNumber_ second number for the addition
+     * @return result_ result of the sum
+     */
     function addition(uint256 firstNumber_, uint256 secondNumber_) external returns (uint256 result_) {
         result_ = firstNumber_ + secondNumber_;
         result = result_;
@@ -43,7 +51,12 @@ contract CalculatorFoundry {
         emit Addition(firstNumber_, secondNumber_, result_);
     }
 
-    // 2. Substraction
+    /**
+     * @notice Function to substract two numbers
+     * @param firstNumber_ first number for the substraction
+     * @param secondNumber_ second number for the substraction
+     * @return result_ result of the substraction
+     */
     function substraction(uint256 firstNumber_, uint256 secondNumber_) external returns (uint256 result_) {
         result_ = firstNumber_ - secondNumber_;
         result = result_;
@@ -51,8 +64,12 @@ contract CalculatorFoundry {
         emit Substraction(firstNumber_, secondNumber_, result_);
     }
 
-    // 3. Multiply
-
+    /**
+     * @notice Function to multiply two numbers
+     * @param firstNumber_ first number for the multiplication
+     * @param secondNumber_ second number for the multiplication
+     * @return result_ result of the substraction
+     */
     function multiplier(uint256 firstNumber_, uint256 secondNumber_) external onlyAdmin returns (uint256 result_) {
         result_ = firstNumber_ * secondNumber_;
         result = result_;
@@ -60,8 +77,12 @@ contract CalculatorFoundry {
         emit Multiplier(firstNumber_, secondNumber_, result_);
     }
 
-    // 4. Division
-
+    /**
+     * @notice Function to divide two numbers
+     * @param firstNumber_ first number for the division
+     * @param secondNumber_ second number for the division
+     * @return result_ result of the division
+     */
     function division(uint256 firstNumber_, uint256 secondNumber_) external returns (uint256 result_) {
         require(secondNumber_ != 0, "Cannot divide by zero");
         result_ = firstNumber_ / secondNumber_;
